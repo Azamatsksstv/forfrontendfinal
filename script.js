0e6475ac7569a7ead7424567fa548470fb40d29d
Azamatskkstv <azambylbaev@gmail.com>
2021-12-25 03:20:02 +0600

    nadeus last commit

---
+++ script.js
@@ -0,0 +1,71 @@
+function toggleText() {
+  
+    var showMoreText =
+        document.getElementById("moretext");
+
+    if (showMoreText.style.display === "none") {
+
+
+        showMoreText.style.display = "inline";
+        
+    }
+
+    else {
+
+        showMoreText.style.display = "none"
+    }
+}
+function toggleText2() {
+    
+    var showMoreText =
+        document.getElementById("moretext2");
+
+    if (showMoreText.style.display === "none") {
+
+
+        showMoreText.style.display = "inline";
+        
+    }
+
+    else {
+
+        showMoreText.style.display = "none"
+    }
+}
+function toggleText3() {
+    
+    var showMoreText =
+        document.getElementById("moretext3");
+
+    if (showMoreText.style.display === "none") {
+
+
+        showMoreText.style.display = "inline";
+        
+    }
+
+    else {
+
+        showMoreText.style.display = "none"
+    }
+}
+// import Highway from "@dogstudio/highway";
+// import Fade from './js.js';
+
+// const H = new Highway.Core({
+//     transition: {
+//         default: Fade
+//     }
+// });
+
+// const links = document.querySelectorAll('nav a');
+
+// H.on('NAVIGATE IN', ({to, location}) => {
+//     links.forEach(link =>{
+//         link.classList.remove('active');
+
+//         if(link.href === location.href){
+//             link.classList.add('active' );
+//         }
+//     });
+// });
